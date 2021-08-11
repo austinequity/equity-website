1ca436ae3ea1938ecb6bc68ad640cf42df336da9
austin-sanga <austinsanga2000@gmail.com>
2021-08-11 05:47:21 +0300

    removed colorsand stuffs

--- src/app/modules/web/components/projects.component.ts
+++ src/app/modules/web/components/projects.component.ts
@@ -5,7 +5,8 @@ import {Component, AfterViewInit, EventEmitter, Input, OnDestroy, OnInit, Output
 
 @Component({
     selector: 'app-projects',
-    template: `<div class="container-fluid">
+    template: `<div class="container-project-body">
+    <div class="container-fluid">
     <div class="project-image">
         <div class="funded-unfunded">
          
@@ -32,6 +33,68 @@ import {Component, AfterViewInit, EventEmitter, Input, OnDestroy, OnInit, Output
         <div class="view-project">
             
         </div>
+</div>
+
+
+
+<div class="container-fluid">
+    <div class="project-image">
+        <div class="funded-unfunded">
+         
+        </div>
+    </div>
+    <div class="container-title-budget">
+            <div class="container-title">
+                
+            </div>
+
+            <div class="container-budget">
+
+            </div>
+    </div>
+    <div class="container-prog-perc">
+        <div class="container-progress-bar">
+
+        </div>
+
+        <div class="percentage-details">
+
+        </div>
+    </div>
+        <div class="view-project">
+            
+        </div>
+</div>
+
+
+<div class="container-fluid">
+    <div class="project-image">
+        <div class="funded-unfunded">
+         
+        </div>
+    </div>
+    <div class="container-title-budget">
+            <div class="container-title">
+                
+            </div>
+
+            <div class="container-budget">
+
+            </div>
+    </div>
+    <div class="container-prog-perc">
+        <div class="container-progress-bar">
+
+        </div>
+
+        <div class="percentage-details">
+
+        </div>
+    </div>
+        <div class="view-project">
+            
+        </div>
+</div>
 </div>`,
     styleUrls: ['../styles/project-container-fluid.style.scss']
 })
