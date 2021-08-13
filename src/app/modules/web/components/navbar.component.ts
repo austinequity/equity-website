6eb7b6aeec9ae72ce8751814377fe675eb69d762
austin-sanga <austinsanga2000@gmail.com>
2021-08-13 10:03:54 +0300

    progress responsive navbar

--- src/app/modules/web/components/navbar.component.ts
+++ src/app/modules/web/components/navbar.component.ts
@@ -5,16 +5,59 @@ import {Component, AfterViewInit, EventEmitter, Input, OnDestroy, OnInit, Output
 
 @Component({
     selector: 'app-navbar',
-    template: `<nav>
-    <label><a class="title" href="index.html">AUSTIN EQUITY</a></label>
-    <ul class="topnav">
-        <li><a class="othert" routerLink='/projects/:category'>projects</a></li>
-        <li><a class="othert" routerLink='/howitworks'>How it works?</a></li>
-        <li><a class="othert" routerLink='/about'>ABOUT</a></li>
-        <li><a class="othert" routerLink='/login'>LOGIN</a></li>
-        <li><a class="othert" routerLink='/register'>REGISTER</a></li>
-    </ul>
-</nav>`,
+    template: `<header class="header">
+    <nav class="navbar">
+        <label><a class="title" href="index.html">AUSTIN EQUITY</a>
+        </label>
+            <ul class="topnav">
+                <li class="nav-item" >
+                    <a class="nav-link" routerLink='/projects/:category'>projects</a>
+                </li>
+                <li class="nav-item" >
+                    <a class="nav-link" routerLink='/howitworks'>How it works?</a>
+                </li>
+                <li class="nav-item" >
+                    <a class="nav-link" routerLink='/about'>ABOUT</a>
+                </li>
+                <li class="nav-item" >
+                    <a class="nav-link" routerLink='/login'>LOGIN</a>
+                </li>
+                <li class="nav-item" >
+                    <a class="nav-link" routerLink='/register'>REGISTER</a>
+                </li>
+            </ul>
+            <div class="humburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+    </nav>
+</header>
+
+<!-- <header class="header">
+        <nav class="navbar">
+            <a href="#" class="nav-logo">WebDev.</a>
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+    </header> -->`,
     styleUrls: ['../styles/navbar.style.scss']
 })
 export class NavbarComponent implements OnInit, OnDestroy, AfterViewInit {
