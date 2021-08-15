66d5d52cbe82fc02bad77636b55a8d5cf134a4c1
austin-sanga <austinsanga2000@gmail.com>
2021-08-15 21:35:09 +0300

    made angular begining comments

--- src/app/modules/web/web.module.ts
+++ src/app/modules/web/web.module.ts
@@ -2,8 +2,9 @@ import {bfast} from 'bfastjs';
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
-import {Routes} from '@angular/router';
+import {Routes} from '@angular/router'; 
 import {ROUTES} from '@angular/router';
+// import {MatButtonModule} from '@angular/material';
 
 
 import {BannerComponent} from './components/banner.component';
@@ -73,6 +74,7 @@ const routes: Routes = [
         }
       ]
     },
+    // MatButtonModule
     
   ],
   exports: [
