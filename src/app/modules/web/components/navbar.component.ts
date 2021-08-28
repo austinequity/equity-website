62e62bffb41de28dec5e1a74d1fd649b82a61501
austin-sanga <austinsanga2000@gmail.com>
2021-08-28 13:13:24 +0300

    updates

--- src/app/modules/web/components/navbar.component.ts
+++ src/app/modules/web/components/navbar.component.ts
@@ -9,69 +9,28 @@ import {Component, AfterViewInit, EventEmitter, Input, OnDestroy, OnInit, Output
     <nav class="navbar">
         <label><a class="title" routerLink='/index'>AUSTIN EQUITY</a>
         </label>
-            <ul class="topnav">
-                <li class="nav-item" >
-                    <a class="nav-link" routerLink='/projects/:category'>projects</a>
-                </li>
-                <li class="nav-item" >
-                    <a class="nav-link" routerLink='/howitworks'>How it works?</a>
-                </li>
-                <li class="nav-item" >
-                    <a class="nav-link" routerLink='/about'>ABOUT</a>
-                </li>
-                <li class="nav-item" >
-                    <a class="nav-link" routerLink='/login'>LOGIN</a>
-                </li>
-                <li class="nav-item" >
-                    <a class="nav-link" routerLink='/register'>REGISTER</a>
-                </li>
-            </ul>
-            <div class="humburger" tabindex="0" mat-button (click)="drawer.toggle()">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-            
-  <mat-drawer #drawer class="example-sidenav" mode="side">
-    <p><a class="nav-link" routerLink='/projects/:category'>projects</a></p>
-    <!-- <p><a class="nav-link" routerLink='/howitworks'>How it works?</a></p>
-    <p><a class="nav-link" routerLink='/about'>ABOUT</a></p>
-    <p><a class="nav-link" routerLink='/login'>LOGIN</a></p>
-    <p><a class="nav-link" routerLink='/register'>REGISTER</a></p> -->
-
-  </mat-drawer>
-
-  
-
-
+        <ul class="topnav">
+            <li class="nav-item">
+                <a class="nav-link" routerLink='/projects/:category'>projects</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" routerLink='/howitworks'>How it works?</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" routerLink='/about'>ABOUT</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" routerLink='/login'>LOGIN</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" routerLink='/register'>REGISTER</a>
+            </li>
+        </ul>
+        <div class="humburger">
+             <button >☰ </button>
+        </div>
     </nav>
-</header>
-
-<!-- <header class="header">
-        <nav class="navbar">
-            <a href="#" class="nav-logo">WebDev.</a>
-            <ul class="nav-menu">
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Blog</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">About</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Contact</a>
-                </li>
-            </ul>
-            <div class="hamburger">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-        </nav>
-        
-    </header> -->`,
+</header>`,
     styleUrls: ['../styles/navbar.style.scss']
 })
 export class NavbarComponent implements OnInit, OnDestroy, AfterViewInit {
