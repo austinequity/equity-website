b18e694d3061e2e1cf72e9c1bc97fc343590e040
austin-sanga <austinsanga2000@gmail.com>
2021-09-01 16:46:57 +0300

    backend update

--- src/app/modules/web/components/projects.component.ts
+++ src/app/modules/web/components/projects.component.ts
@@ -1,5 +1,6 @@
 import {bfast, BFast} from 'bfastjs';
 import {Component, AfterViewInit, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
+import {ProjectState} from '../states/project.state';
 
 
 
@@ -14,19 +15,21 @@ import {Component, AfterViewInit, EventEmitter, Input, OnDestroy, OnInit, Output
 
 
 
-    <div class="container-fluid">
+    <div *ngFor="let p of projectState.project | async" class="container-fluid">
         <div class="project-image">
             <div class="funded-unfunded">
+
             </div>
+            <img style="height:300px; width: 100%; background: #f5f5f5" src="{{p.image}}">
         </div>
 
         <div class="container-title-budget">
             <div class="container-title">
-                <h1 class="project-title">Mafinga Eucalyptus plantation.</h1>
+                <h1 class="project-title">{{p.product}}</h1>
             </div>
 
             <div class="container-budget">
-                 <h1 class="project-budget">Project budget:</h1>
+                 <h1 class="project-budget">Project budget: {{p.purchase | currency:'Tsh '}}</h1>
             </div>
         </div>
 
@@ -55,11 +58,12 @@ export class ProjectsComponent implements OnInit, OnDestroy, AfterViewInit {
 
     
     
-    constructor(){
+    constructor(public readonly projectState: ProjectState){
     }
     
     async ngOnInit(): Promise<any> {
-        
+        //your codes
+this.projectState.all();
     }
 
     async ngAfterViewInit(): Promise<any> {
