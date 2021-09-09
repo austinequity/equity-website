30539188a3901787f171c62ea0f3c351fe1ea18e
austin-sanga <austinsanga2000@gmail.com>
2021-09-09 22:46:53 +0300

    comments on mydashboard

--- src/app/modules/web/components/mydashboard.component.ts
+++ src/app/modules/web/components/mydashboard.component.ts
@@ -8,14 +8,25 @@ import {Component, AfterViewInit, EventEmitter, Input, OnDestroy, OnInit, Output
     template: `<div class="body">
     <div class="wrapper">
         <div class="investmentSummary">
+            <!-- -balance ni total of physical cash available
+            -hii ndo ambayo iko withdrawable
+            -hii inaongezeka kama interest(inabidi product iwe na uwezo wa kuallocatiwa withdrawable interest) zimekua translated to withdrawable cash
+            -also inaongezeka in sense ya deposit ikifanyika
+            -Inapungua in incidence hela imetumika kuinvest
+            -inapungua incase of withdraw -->
             <div class="balance-bookValue-totalEarning">
                 <span class="upTab-balance-figure">Tzs 0</span>
                 <span class="upTab-data">Balance</span>
             </div>
+            <!-- -book Value hii inasoma hypothetical valuation
+            -Kwenye product c kuna sehemu ya kuweka valuation so ile ndo inakuja hapa(lakini inkua inafanya math ili valuation ya hapa iwe reflective na shares za user)
+            -math inayofanyawa ni shares za mteja over total shares time valuation
+            -(keeping note valuation kila ikiongezwa inakeep time stmap, so ata kwa user itabadilika based on the stamp(meaning duration aliyopo)) -->
             <div class="balance-bookValue-totalEarning">
                 <span class="upTab-blookValue-figure">Tzs 0</span>
                 <span class="upTab-data">Book value</span>
             </div>
+            <!-- -Hii total earning to which inadeduct bookvalue - investments value ya huyo user -->
             <div class="balance-bookValue-totalEarning">
                 <span class="upTab-totalEarning-figure">Tzs 0</span>
                 <span class="upTab-data">Total earnings</span>
@@ -23,6 +34,9 @@ import {Component, AfterViewInit, EventEmitter, Input, OnDestroy, OnInit, Output
             </div>
             
             <!-- lower tab -->
+
+            <!-- for now ilivokaa its if mtu hana initial investment yyte -->
+            <!-- lakini mtu akifanya itabidi iingie table ya angular itakayo display summary investments -->
            <div class="myInvestment-noFund">
             <h1>My Investment</h1>
             <div class="no-fund-box">
